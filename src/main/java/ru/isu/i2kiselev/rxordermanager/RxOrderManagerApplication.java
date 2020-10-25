b8ce31b2813f0c7bf877cf669c7272a5a6746c22
i2kiselev <griggs1299@gmail.com>
2020-10-25 14:32:33 +0800

    Initial commit

---
+++ src/main/java/ru/isu/i2kiselev/rxordermanager/RxOrderManagerApplication.java
@@ -0,0 +1,13 @@
+package ru.isu.i2kiselev.rxordermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxOrderManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RxOrderManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/isu/i2kiselev/rxordermanager/RxOrderManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.isu.i2kiselev.rxordermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RxOrderManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

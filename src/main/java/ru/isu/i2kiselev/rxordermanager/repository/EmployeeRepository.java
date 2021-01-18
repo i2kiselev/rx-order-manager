0b8ee2e7ec7d0b3267472a71473f7c8f70e06fb7
i2kiselev <griggs1299@gmail.com>
2021-01-18 13:54:29 +0800

    Added methods for adding/deleting task estimates for employees

--- src/main/java/ru/isu/i2kiselev/rxordermanager/repository/EmployeeRepository.java
+++ src/main/java/ru/isu/i2kiselev/rxordermanager/repository/EmployeeRepository.java
@@ -4,14 +4,23 @@ import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import ru.isu.i2kiselev.rxordermanager.model.Employee;
 
 @Repository
 public interface EmployeeRepository extends ReactiveCrudRepository<Employee, Integer> {
 
-    @Query("select * from public.employee where $1 = any (tasks)")
+    @Query("select employee.id, employee.username " +
+            "from employee " +
+            "left join employee_task_estimates on " +
+            "employee.id=employee_task_estimates.employee_id " +
+            "where task_id=$1")
     Flux<Employee> findAllByTaskId(Integer task_id);
 
+    @Query("insert into employee_task_estimates values($1,$2,$3)")
+    Mono<Void> addTaskEstimateToEmployee(Integer employee_id, Integer task_id, Integer estimate);
 
+    @Query("delete from employee_task_estimates where employee_task_estimates.employee_id=$1 and employee_task_estimates.task_id=$2")
+    Mono<Void> removeTaskEstimateToEmployee(Integer employee, Integer task_id);
 
 }

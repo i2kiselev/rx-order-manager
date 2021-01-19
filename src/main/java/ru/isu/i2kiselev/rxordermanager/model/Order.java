d8bc4f457ad40289b19cdccd2146c692117d70ed
i2kiselev <griggs1299@gmail.com>
2021-01-19 17:35:15 +0800

    Small Order.class fixes

--- src/main/java/ru/isu/i2kiselev/rxordermanager/model/Order.java
+++ src/main/java/ru/isu/i2kiselev/rxordermanager/model/Order.java
@@ -1,13 +1,17 @@
 package ru.isu.i2kiselev.rxordermanager.model;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 /**
  * Order POJO
- * @version 0.1
+ * @version 0.2
  * @author Ilya Kiselev
  */
 
+@Data
+@Table
 public class Order {
     @Id
     private Integer id;

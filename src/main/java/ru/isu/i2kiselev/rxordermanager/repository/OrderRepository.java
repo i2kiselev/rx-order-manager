9c5066c2ccd93af916642a936bfa4b2e8e46920c
i2kiselev <griggs1299@gmail.com>
2021-01-20 18:28:15 +0800

    Added method for adding tasks to orders

--- src/main/java/ru/isu/i2kiselev/rxordermanager/repository/OrderRepository.java
+++ src/main/java/ru/isu/i2kiselev/rxordermanager/repository/OrderRepository.java
@@ -1,10 +1,16 @@
 package ru.isu.i2kiselev.rxordermanager.repository;
 
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 import ru.isu.i2kiselev.rxordermanager.model.Order;
 
 @Repository
 public interface OrderRepository extends ReactiveCrudRepository<Order,Integer> {
+
+    @Query("insert into order_table values ($1,$2)")
+    Mono<Void> addTaskToOrderByOrderId(Integer orderId, Integer taskId);
+
 
 }

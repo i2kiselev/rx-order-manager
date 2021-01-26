f50ba3ac4cbbbd7f6c10ce013cf3c10f435f3632
i2kiselev <griggs1299@gmail.com>
2021-01-26 13:31:49 +0800

    Added TaskEstimate class

---
+++ src/main/java/ru/isu/i2kiselev/rxordermanager/model/TaskEstimate.java
@@ -0,0 +1,22 @@
+package ru.isu.i2kiselev.rxordermanager.model;
+
+import lombok.Data;
+import org.springframework.data.relational.core.mapping.Table;
+
+/**
+ * Task estimate POJO.
+ * @version 0.1
+ * @author Ilya Kiselev
+ */
+
+@Data
+@Table
+public class TaskEstimate {
+    private Integer taskId;
+
+    private Integer employeeId;
+
+
+    private Integer estimate;
+
+}

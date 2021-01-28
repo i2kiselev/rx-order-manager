71a66d6b630e49d4b44099d322768c422259d3ec
i2kiselev <griggs1299@gmail.com>
2021-01-28 12:57:13 +0800

    Added more tests for EmployeeService

--- src/test/java/ru/isu/i2kiselev/rxordermanager/service/EmployeeServiceTest.java
+++ src/test/java/ru/isu/i2kiselev/rxordermanager/service/EmployeeServiceTest.java
@@ -16,6 +16,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 class EmployeeServiceTest {
@@ -39,6 +40,29 @@ class EmployeeServiceTest {
                             return true;
                     })
                     .verifyComplete();
+    }
+
+    @Test
+    void findByIdReturnsEmployeeMonoTest(){
+        Employee employee = new Employee();
+        employee.setUsername("Test name");
+        given(employeeRepository.findById(any(Integer.class))).willReturn(Mono.just(employee));
+        Mono<Employee> employeeMono = employeeService.findById(1);
+        StepVerifier.create(employeeMono)
+                .thenConsumeWhile(result -> {
+                    assertThat(result).isNotNull();
+                    assertThat(result.getUsername()).isEqualTo(employee.getUsername());
+                    return true;
+                })
+                .verifyComplete();
+
+    }
+
+    @Test
+    void deleteEmployeeInvokesRepositoryDeleteMethod(){
+        given(employeeRepository.delete(any(Employee.class))).willReturn(Mono.when());
+        employeeService.deleteEmployeeById(1);
+        verify(employeeRepository).deleteById(1);
     }
 
     @Test
@@ -69,26 +93,10 @@ class EmployeeServiceTest {
     }
 
     @Test
-    void findByTaskIdReturnsFluxWithEmployeesTest(){
+    void findAllByTaskIdReturnsFluxWithNoEmployeesTest(){
         List<Employee> employees = new ArrayList<>();
-        Employee employee1 = new Employee("user1");
-        Employee employee2 = new Employee("user2");
-        employees.add(employee1);
-        employees.add(employee2);
         given(employeeRepository.findAllByTaskId(any(Integer.class))).willReturn(Flux.fromIterable(employees));
-        Flux<Employee> employeeFlux = employeeService.findAll();
-        StepVerifier.create(employeeFlux)
-                .expectNext(employee1,employee2)
-                .expectNextCount(0)
-                .expectComplete()
-                .verify();
-    }
-
-    /*@Test
-    void addTaskEstimateReturnsMonoVoidTest(){
-        List<Employee> employees = new ArrayList<>();
-        given(employeeRepository.addTaskEstimateToEmployee(any(Integer.class),any(Integer.class),any(Integer.class))).willReturn(Mono.);
-        Flux<Employee> employeeFlux = employeeService.findAll();
+        Flux<Employee> employeeFlux = employeeService.findAllByTaskId(1);
         StepVerifier.create(employeeFlux)
                 .expectNextCount(0)
                 .expectComplete()
@@ -96,13 +104,41 @@ class EmployeeServiceTest {
     }
 
     @Test
-    void findAllByTaskIdReturnsFluxWithNoEmployeesTest(){
+    void findAllByTaskIdReturnsFluxWithEmployeesTest(){
         List<Employee> employees = new ArrayList<>();
+        Employee employee1 = new Employee("user1");
+        Employee employee2 = new Employee("user2");
+        employees.add(employee1);
+        employees.add(employee2);
         given(employeeRepository.findAllByTaskId(any(Integer.class))).willReturn(Flux.fromIterable(employees));
-        Flux<Employee> employeeFlux = employeeService.findAll();
+        Flux<Employee> employeeFlux = employeeService.findAllByTaskId(1);
         StepVerifier.create(employeeFlux)
+                .expectNext(employee1,employee2)
                 .expectNextCount(0)
                 .expectComplete()
                 .verify();
-    }*/
+    }
+
+    @Test
+    void addTaskEstimateReturnsMonoInteger(){
+        List<Employee> employees = new ArrayList<>();
+        given(employeeRepository.addTaskEstimateToEmployee(any(Integer.class),any(Integer.class),any(Integer.class))).willReturn(Mono.just(1));
+        Mono<Integer> addTaskEstimate = employeeService.addTaskEstimate(1,1,1);
+        StepVerifier.create(addTaskEstimate)
+                .expectNextCount(1)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void updateTaskEstimateReturnsMonoInteger(){
+        List<Employee> employees = new ArrayList<>();
+        given(employeeRepository.updateTaskEstimateOfEmployee(any(Integer.class),any(Integer.class),any(Integer.class))).willReturn(Mono.just(1));
+        Mono<Integer> updateTaskEstimate = employeeService.updateTaskEstimate(1,1,1);
+        StepVerifier.create(updateTaskEstimate)
+                .expectNextCount(1)
+                .expectComplete()
+                .verify();
+    }
+
 }

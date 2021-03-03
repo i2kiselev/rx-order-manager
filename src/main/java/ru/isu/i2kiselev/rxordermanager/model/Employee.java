9f34a48f62c9dc6a3822401d92e8159ad3eedb87
i2kiselev <griggs1299@gmail.com>
2021-03-03 11:49:43 +0800

    Employee now implements UserDetails

--- src/main/java/ru/isu/i2kiselev/rxordermanager/model/Employee.java
+++ src/main/java/ru/isu/i2kiselev/rxordermanager/model/Employee.java
@@ -4,6 +4,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
 
 /**
  * Employee POJO. It represent company's employee with array of tasks and respective completion estimates stored in RDBMS
@@ -16,15 +21,49 @@ import org.springframework.data.relational.core.mapping.Table;
 @Data
 @NoArgsConstructor
 @Table("employee")
-public class Employee {
+public class Employee implements UserDetails {
 
     @Id
     private Integer id;
 
     private String username;
 
+    private String password;
+
+    private String role;
+
     public Employee(String username) {
         this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new Role(role));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
 }

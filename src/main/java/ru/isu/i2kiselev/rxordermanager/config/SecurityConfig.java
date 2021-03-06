716f373fe2226a32de563367d31be3c1fa995058
i2kiselev <griggs1299@gmail.com>
2021-03-06 12:21:16 +0800

    Added password encoder

--- src/main/java/ru/isu/i2kiselev/rxordermanager/config/SecurityConfig.java
+++ src/main/java/ru/isu/i2kiselev/rxordermanager/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
 
@@ -14,6 +16,7 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
+                .csrf().disable()
                 .authorizeExchange()
                 .pathMatchers("/manage/**","/task/**","/employee/**")
                 .hasAuthority("ROLE_ADMIN")
@@ -24,10 +27,16 @@ public class SecurityConfig {
                 .and()
                     .formLogin()
                     .loginPage("/login")
-                    .authenticationSuccessHandler(new RedirectServerAuthenticationSuccessHandler("/"))
+                    .authenticationSuccessHandler(new RedirectServerAuthenticationSuccessHandler("/employee"))
                 .and()
                 .logout()
                 .logoutUrl("/");
         return http.build();
     }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }

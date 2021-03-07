1c2b4dffeaa4f78328d8c196c869e0b8c4517be6
i2kiselev <griggs1299@gmail.com>
2021-03-07 21:53:18 +0800

    Added authentication success redirects depending on employee role

--- src/main/java/ru/isu/i2kiselev/rxordermanager/config/SecurityConfig.java
+++ src/main/java/ru/isu/i2kiselev/rxordermanager/config/SecurityConfig.java
@@ -2,15 +2,19 @@ package ru.isu.i2kiselev.rxordermanager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.WebFilterExchange;
 import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
+import org.springframework.security.web.server.savedrequest.WebSessionServerRequestCache;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
 
 @Configuration
-@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
@@ -18,16 +22,16 @@ public class SecurityConfig {
         http
                 .csrf().disable()
                 .authorizeExchange()
-                .pathMatchers("/manage/**","/task/**","/employee/**")
+                .pathMatchers("/manager/**","/task/**","/employee/**")
                 .hasAuthority("ROLE_ADMIN")
-                .pathMatchers("/task-panel/")
+                .pathMatchers("/task-panel/**")
                 .hasAuthority("ROLE_EMPLOYEE")
-                .pathMatchers("/**")
+                .pathMatchers("/login","/register","/css/**","/js/**","/img/**")
                 .permitAll()
                 .and()
                     .formLogin()
                     .loginPage("/login")
-                    .authenticationSuccessHandler(new RedirectServerAuthenticationSuccessHandler("/employee"))
+                    .authenticationSuccessHandler(redirectServerAuthenticationSuccessHandler())
                 .and()
                 .logout()
                 .logoutUrl("/");
@@ -39,4 +43,20 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public RedirectServerAuthenticationSuccessHandler redirectServerAuthenticationSuccessHandler() {
+        RedirectServerAuthenticationSuccessHandler successHandler = new RedirectServerAuthenticationSuccessHandler() {
+            @Override
+            public Mono<Void> onAuthenticationSuccess(WebFilterExchange webFilterExchange, Authentication authentication) {
+                if (authentication.getAuthorities().iterator().next().getAuthority().equals("ROLE_ADMIN")) {
+                    this.setLocation(URI.create("/employee"));
+                } else {
+                    this.setLocation(URI.create("/manager/task-panel/"));
+                }
+                return super.onAuthenticationSuccess(webFilterExchange, authentication);
+            }
+        };
+        successHandler.setRequestCache(new WebSessionServerRequestCache());
+        return successHandler;
+    }
 }

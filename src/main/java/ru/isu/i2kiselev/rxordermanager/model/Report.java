c85d68c7a751840546d3f1816fe9674e45b9fff4
i2kiselev <griggs1299@gmail.com>
2021-06-14 15:10:25 +0800

    .

---
+++ src/main/java/ru/isu/i2kiselev/rxordermanager/model/Report.java
@@ -0,0 +1,18 @@
+package ru.isu.i2kiselev.rxordermanager.model;
+
+import lombok.Data;
+
+@Data
+public class Report {
+
+    private String employeeName;
+
+    private String feedback;
+
+    private Double duration;
+
+    private Double estimateDuration;
+
+    private String orderName;
+
+}
